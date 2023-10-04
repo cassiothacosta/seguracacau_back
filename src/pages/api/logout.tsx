767bf1a4c9ad0f7f0cf230a6520919a56ca1c891
cassio.costa.dev@gmail.com <cassio.costa.dev@gmail.com>
2023-10-03 21:29:00 -0300

    fix logout api

--- src/pages/api/logout.tsx
+++ src/pages/api/logout.tsx
@@ -2,6 +2,6 @@ import { removeTokenCookie } from '../../lib/auth-cookies'
 
 export default async function logout(req: any, res: any) {
   removeTokenCookie(res)
-  res.writeHead(302, { Location: req.headers.referer+'/login'})
+  res.writeHead(302, { Location: req.headers.referer+'/'})
   res.end()
 }
